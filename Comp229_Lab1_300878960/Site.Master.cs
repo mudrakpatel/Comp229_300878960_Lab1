4eebe7eb1936cbf0389a3099d0a3a23bdd67072c
mudrakpatel <mudrakpatel96@gmail.com>
2016-09-11 04:58:03 -0500

    First commit

--- Comp229_Lab1_300878960/Site.Master.cs
+++ Comp229_Lab1_300878960/Site.Master.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace Comp229_Lab1_300878960
@@ -11,7 +12,8 @@ namespace Comp229_Lab1_300878960
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            var h1 = new HtmlGenericControl("h1");
+            h1.InnerHtml = "Mudrak Patel here!";
         }
     }
 }
